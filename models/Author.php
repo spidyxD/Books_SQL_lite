5babcf8c8cc61ffbf2ec416d2f4466770bdac1f4
spidyxD <addielo.oamvi@gmail.com>
2021-04-30 23:49:42 -0600

    fixes

--- models/Author.php
+++ models/Author.php
@@ -1,24 +1,8 @@
 <?php
-  // file: author.php
+  // file: models/Author.php
 
 class Author extends Model {
 
-  static $authors = [
-    ['id'=>1,'author'=>'Abraham Silberschatz','nationality'=>'Israelis / American','books__title'=>'Operating System Concepts','birth_year'=>'1952',
-    'books__book_id'=>'1','fields'=>'Database Systems, Operating Systems'], 
-   
-    ['id'=>2,'author'=>'Andrew S. Tanenbaum','nationality'=>'Dutch / American','books__title'=>'Modern Operating Systems','birth_year'=>'1944',
-    'books__book_id'=>'1','fields'=>'Database Systems, Operating Systems']
-  ];
-
-  public static function all() { 
-    return self::$authors; 
-  }
-
-  public static function find($id) {
-    foreach (self::$authors as $key => $author)
-      if ($author['id'] == $id) return $author;
-    return [];
-  }
+  protected static $table = 'author';
 }
 ?>
